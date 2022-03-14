b65cf551c39e3815b249aa4142266707f424da3f
Philip Garcia <philip_garcia1@baylor.edu>
2022-03-14 15:42:53 -0500

    gui stuff

---
+++ demo4_output_as_label.py
@@ -0,0 +1,63 @@
+import tkinter
+import tkinter.messagebox
+
+
+class kiloconverter:
+    def __init__(self):
+        self.main_window = tkinter.Tk()
+
+        self.main_window.geometry("500x200")
+
+        self.top_frame = tkinter.Frame(self.main_window)
+        self.mid_frame = tkinter.Frame(self.main_window)
+        self.bottom_frame = tkinter.Frame(self.main_window)
+
+        self.prompt_label = tkinter.Label(
+            self.top_frame, text="Enter a distance in Kilometers:"
+        )
+
+        self.prompt_label.pack(side="top")
+
+        self.top_frame.pack(side="top")
+        self.mid_frame.pack(side="top")
+        self.bottom_frame.pack(side="top")
+
+        self.kilo_entry = tkinter.Entry(self.top_frame, width=10)
+
+        self.prompt_label.pack(side="left")
+        self.kilo_entry.pack(side="left")
+
+        self.descr_label = tkinter.Label(self.mid_frame, text="Converted to miles: ")
+
+        self.miles_var = tkinter.StringVar()
+
+        self.miles_label = tkinter.Label(self.mid_frame, textvariable=self.miles_var)
+
+        self.descr_label.pack(side="left")
+        self.miles_label.pack(side="left")
+
+        self.mybutton = tkinter.Button(
+            self.main_window, text="Convert", command=self.convert
+        )
+
+        self.quit_button = tkinter.Button(
+            self.main_window, text="Quit", command=self.main_window.destroy
+        )
+
+        self.mybutton.pack(side="left")
+
+        self.quit_button.pack(side="right")
+
+        tkinter.mainloop()
+
+    def convert(self):
+        kilo = float(self.kilo_entry.get())
+
+        miles = round(kilo * 0.6214, 2)
+
+        self.miles_var.set(miles)
+
+
+kilo_conv = kiloconverter()
+
+print("moving on.....")
